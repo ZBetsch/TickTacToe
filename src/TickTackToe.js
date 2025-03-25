455a75a2c12ccc3bf7b136bc41c250d4d189cffd
Zach Betsch <betschzt@mail.uc.edu>
2025-03-25 12:36:35 -0400

    Commented out debugging console log

--- src/TickTackToe.js
+++ src/TickTackToe.js
@@ -1,6 +1,6 @@
 function getWinningLines(gridsize){
     const lines=[];
-
+    
     for(let row = 0 ; row < gridsize; row++){
         const rowLine = [];
         for (let col = 0; col < gridsize; col++){
@@ -28,8 +28,8 @@ function getWinningLines(gridsize){
         rightDiag.push(i * gridsize + (gridsize - 1 - i));
     }
     lines.push(rightDiag);
-    
-    console.log(lines);
+    // Debugging line
+    // console.log(lines);
     return lines;
 }
 
